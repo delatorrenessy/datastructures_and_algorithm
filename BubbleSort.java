8dbe6bb1a3c1f175903ef3d27b3d42420dfe704a
Nessy Dela Torre <comsci.delatorre.nessy@gmail.com>
2025-11-10 13:04:31 +0800

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,33 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1};
+        bubbleSort(arr);
+
+        System.out.println("Sorted Array (Bubble Sort):");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were swapped, array is sorted
+            if (!swapped) break;
+        }
+    }
+}
