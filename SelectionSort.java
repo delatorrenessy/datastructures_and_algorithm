35bc4261a5d0c3b2af2abab89cd3935ed5879b23
Nessy Dela Torre <comsci.delatorre.nessy@gmail.com>
2025-11-10 13:04:51 +0800

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,31 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1};
+        selectionSort(arr);
+
+        System.out.println("Sorted Array (Selection Sort):");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            // Find the minimum element in the unsorted part
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
